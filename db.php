b3059807211cdac7fa901e730991c6fa8180e719
Angie <liya0706s@gmail.com>
2023-11-17 14:23:28 +0800

    feat:使用函式來取出資料表的資料，在函式中增加參數，提升泛用性

--- db.php
+++ db.php
@@ -2,19 +2,27 @@
 
 // 呼叫 all() 函式，
 // 並將函式返回的值（所有學生資料的陣列）賦值給變數 $rows
+// ???
 $rows=all();
 
 // 函式輸出
 dd($rows);
 
 // 定義函式 all() 用來取得學生資料
-function all(){
+// 為什麼這邊要=null?
+function all($table=null){
     $dsn="mysql:host=localhost;charset=utf8;dbname=school";
     $pdo=new PDO($dsn, 'root', '');
 
-    $sql="select * from `students` ";
+    // table名稱如果被惡搞是空值或是沒有該名稱，要先判斷有這個資料表，且不是空的
+    if(isset($table) && !empty($table)){
+        $sql="select * from `$table` ";
+    // 資料表名稱可以改成$table
     $rows=$pdo->query($sql)->fetchAll();
     return $rows;
+    }else{
+        echo "錯誤:沒有指定的資料表名稱";
+    }    
 }
 
 function dd($array){
