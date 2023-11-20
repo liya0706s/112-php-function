cda2b42fc551d5fdf745f9af87608fac91bf485c
Angie <liya0706s@gmail.com>
2023-11-20 11:32:40 +0800

    feat:del()完成

--- db.php
+++ db.php
@@ -1,3 +1,4 @@
+<h1>自訂函式CRUD</h1>
 <?php
 
 // 呼叫 all() 函式，
@@ -5,27 +6,32 @@
 // 該情況下的像是，假設x=5;
 
 // 2023-11-17 定義函式all()
-$rows = all('students',['dept'=>'1','graduate_at'=>'23']);
+// $rows = all('students', ['dept' => '1', 'graduate_at' => '23']);
 
 // find()-會回傳資料表 指定id的資料，因為很常使用
 // 指定條件之後的那一筆資料還有，例如唯一特定的帳號密碼
 // 2023-11-20 find() 指定id的函式
 // 指定條件中，只要一筆資料的還有什麼???-->帳號密碼，或是比較私人資料庫中只有唯一性的
-$row=find('students',['dept'=>'1','graduate_at'=>'23']);
+// $row = find('students', ['dept' => '1', 'graduate_at' => '23']);
 
-echo "<h3>相同條件使用 all()</h3>";
+// echo "<h3>相同條件使用 all()</h3>";
 // 呼叫函式輸出
-dd($rows);
-echo "<hr>";
-echo "<h3>相同條件使用 find()</h3>";
-dd($row);
+// dd($rows);
+// echo "<hr>";
+// echo "<h3>相同條件使用 find()</h3>";
+// dd($row);
 
 // 2023-11-20 update()
 // $up=update("students", '3',['dept'=>'16','name'=>'漲明珠']);
 // $up = update("students", '3', ['dept' => '16', 'name' => '漲明珠']);
-
-
 // dd($up);
+
+// 2023-11-20 del()
+del('students',['dept'=>'5','status_code'=>'001']);
+// 呼叫帶入參數
+
+// 2023-11-20 insert()
+// insert('dept', ['code' => '1112', 'name' => '織品系']);
 
 // 定義函式 all() 用來取得學生資料
 // null 是要有參數，可以是空的
@@ -58,11 +64,11 @@ function all($table = null, $where = '', $other = '')
 
         } else {
             // 當他是字串，原句子
-            $sql .=" $where";
+            $sql .= " $where";
             // 資料表名稱可以改成$table參數
         }
-            $sql .= $other;
-        echo 'all=>'.$sql;
+        $sql .= $other;
+        echo 'all=>' . $sql;
         $rows = $pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
         // 整合所有語句
         // associate"關聯"名稱變成只拿 欄位名稱，不拿索引
@@ -141,6 +147,65 @@ function update($table, $id, $cols)
     // 結果只有數字(代表影響了幾列)
 
     // join...
+}
+
+// 2023-11-20 insert() 新增資料 語法: select
+function insert($table, $values){
+    // $values參數代表，新增欄位的陣列
+    $dsn = "mysql:host=localhost;charset=utf8;dbname=school";
+    $pdo = new PDO($dsn, 'root', '');
+
+    $sql = "insert into `$table` ";
+    // 欄位和值
+    $cols = "(`" . join("`,`", array_keys($values)) . "`)";
+    // 取鍵的(key)名稱array_keys
+
+    $vals = "('" . join("','", $values) . "')";
+
+    // $cols="(``,``,``,``)";
+    // $vals="(``,``,``,``)";
+
+    $sql = $sql . $cols . " values " . $vals;
+
+    // 帶入以上的值展開:
+    // $sql = "insert into `$table` " . "(``,``,``,``)" ." values " . "(``,``,``,``)";
+
+    return $pdo->exec($sql);
+}
+
+// 2023-11-20 del() 刪除指令 可以刪除一筆或是多筆資料:
+// del() 類似find()
+function del($table,$id){
+    // 判斷參數id是不是陣列
+    $dsn = "mysql:host=localhost;charset=utf8;dbname=school";
+    $pdo = new PDO($dsn, 'root', '');
+    // 要抽出來為了程式撰寫和維護方便，不用改很多次
+
+    $sql="delete from `$table` where";
+    // 把共同的部分拆出來
+    // 有where
+
+    if(is_array($id)){
+        // foreach目的:將陣列的key和value轉成sql需要的字串，
+        // 才可以兜成給資料庫執行的指令
+        foreach($id as $col => $value){
+                $tmp[]="`$col`='$value'";
+                // 陣列轉成字串的顯示方式???
+                // 這邊為什麼又是撇號??不是單引號
+            }
+            $sql .= join(" && ",$tmp);
+    }else if(is_numeric($id)){
+        // 不是陣列，是數字的話
+          $sql .= " `id`='$id'";
+    }else{
+        echo "錯誤:參數的資料型態必須是數字或陣列";
+    }
+    echo $sql;
+    return $pdo->exec($sql);
+
+    // $sql="delete from `$table` where `id`='?'";
+    // $sql="delete from `$table` where `col`='?' && `col2`='?'";
+
 }
 
 // directDump 秀出陣列 用成函式
